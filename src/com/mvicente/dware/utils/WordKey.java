af0ad0f1f77dd1e10c7fab2b969fb4fceba31d16
Anthony Vicente <anthony@mvicente.com>
2016-06-22 18:08:23 -0500

    'finished' implementation of WordKey.java including reading the word list and fetching from wordMap.

--- src/com/mvicente/dware/utils/WordKey.java
+++ src/com/mvicente/dware/utils/WordKey.java
@@ -1,5 +1,8 @@
 package com.mvicente.dware.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class WordKey
@@ -12,8 +15,49 @@ public class WordKey
         readWordList(wordFile);
     }
 
+    public String getWord(int key)
+    {
+        return wordMap.get(key);
+    }
+
+    public String getWord(String key)
+    {
+        String value = null;
+        try
+        {
+            value = wordMap.get(Integer.parseInt(key));
+        }
+        catch (NumberFormatException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return value;
+    }
+
     public void readWordList(String wordFile)
     {
-        System.out.println("TEST");
+        try
+        {
+            BufferedReader breader = new BufferedReader(new FileReader(wordFile));
+            String line;
+            while ((line = breader.readLine()) != null)
+            {
+                String lineArr[] = line.split("\t");
+
+                if(lineArr.length != 2)
+                {
+                    throw new IndexOutOfBoundsException("Improperly formatted word file");
+                }
+
+                String key = lineArr[0];
+                String value = lineArr[1];
+                wordMap.put(Integer.parseInt(key), value);
+            }
+        }
+        catch(IOException|IndexOutOfBoundsException|NumberFormatException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
